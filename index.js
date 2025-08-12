4910f698698f5a913678ca35cc557ab8365cb7f9
foxosdev9 <chentoufanass3@gmail.com>
2025-08-12 16:11:56 +0100

    the last test for deploy

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = ex();
 
 app.use(ex.static('public'));
 
+app.get('/', (req, res) => {
+    res.json({name: 'ANASS DEVELOPER'});
+})
+
 app.listen(port, (req, res) => {
     console.log('SERVER RUN AT : 8080')
 });
